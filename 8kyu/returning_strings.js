84be33060f93bb87f17a2d1465052b86e400234a
StasKlymenko <klymenko.stas@gmail.com>
2023-01-25 23:25:49 +0200

    Add new katas

---
+++ 8kyu/returning_strings.js
@@ -0,0 +1,7 @@
+// ID: 55a70521798b14d4750000a4
+
+const name = "Ryan"
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
---
+++ 8kyu/you_cant_code_under_pressure_#1.js
@@ -0,0 +1,7 @@
+// ID: 53ee5429ba190077850011d4
+
+i = 5
+
+function doubleInteger(i) {
+    return i * 2;
+}
